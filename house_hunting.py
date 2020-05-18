5c726613b5e532ed53273929343e9dd90e7d873a
gillie1022 <dave.gillie@icloud.com>
2020-05-18 14:36:04 -0400

    cleaned up code

--- house_hunting.py
+++ house_hunting.py
@@ -1,12 +1,15 @@
 annual_salary = int(input("Enter your annual salary: "))
-current_savings = 0
+
 portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+
 rate_input = input("Enter the expected rate of return: ")
 if rate_input == "":
     r = .04
 else:
     r = float(rate_input)
+
 total_cost = int(input("Enter the cost of your dream home: "))
+
 p_down_input = input("Enter the percent of your home's cost to save as a down payment: ")
 if p_down_input == "":
     p = .25
@@ -14,6 +17,8 @@ else:
     p = float(p_down_input)
 
 portion_down_payment = int(total_cost * p)
+
+current_savings = 0
 
 num_months = 0
 
